06f359fa713eca3c0d7a63b2b18f5dd3d5bad2b1
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 16:20:05 -0700

    pf: get the test to pass

--- prime-factors/src/main/java/PrimeFactorsSolver.java
+++ prime-factors/src/main/java/PrimeFactorsSolver.java
@@ -5,6 +5,10 @@ public class PrimeFactorsSolver {
     public List<Integer> primeFactors(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
 
+        if (n % 2 == 0) {
+            n = n / 2;
+            factors.add(2);
+        }
         if (n == 2 || n == 3) {
             factors.add(n);
         }
