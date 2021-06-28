0e51cf89adb85d82b73e41f29c3cf50e1f15dc8d
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 14:08:34 -0700

    bp: refactor

--- bowling-pins/src/test/java/BowlingTest.java
+++ bowling-pins/src/test/java/BowlingTest.java
@@ -1,19 +1,25 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingTest {
 
+    private static Bowling bowling;
+
+    @BeforeAll
+    static void setup() {
+        bowling = new Bowling();
+    }
+
     @Test
     void return_field_with_no_pins_removed_when_sending_an_empty_array() {
-        Bowling bowling = new Bowling();
         int[] pinsToRemove = {};
         assertEquals("I I I I\n I I I \n  I I  \n   I   ", bowling.bowlingPins(pinsToRemove));
     }
 
     @Test
     void return_field_with_pin_1_removed_when_sending_an_array_of_1() {
-        Bowling bowling = new Bowling();
         int[] pinsToRemove = {1};
         assertEquals("I I I I\n I I I \n  I I  \n       ", bowling.bowlingPins(pinsToRemove));
     }
