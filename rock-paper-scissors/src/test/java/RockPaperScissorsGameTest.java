4352043c9d919d6fcfc4b48567440217d306e477
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 09:12:35 -0700

    rps: another test added, fails

--- rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
+++ rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
@@ -11,4 +11,10 @@ class RockPaperScissorsGameTest {
         assertEquals("Draw!", game.play("paper", "paper"));
     }
 
+
+    @Test
+    void return_player1_when_player1_is_rock_and_player2_is_scissors() {
+        RockPaperScissorsGame game = new RockPaperScissorsGame();
+        assertEquals("Player 1 won!", game.play("rock", "scissors"));
+    }
 }
