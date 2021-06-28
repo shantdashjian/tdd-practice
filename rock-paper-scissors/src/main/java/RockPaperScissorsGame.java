a5f2328dc8de5a513b5357cfd0af2f76f63785c8
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 09:26:37 -0700

    rps: refactor

--- rock-paper-scissors/src/main/java/RockPaperScissorsGame.java
+++ rock-paper-scissors/src/main/java/RockPaperScissorsGame.java
@@ -1,9 +1,17 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class RockPaperScissorsGame {
     public String play(String player1, String player2) {
-        if (player1.equals("rock") && player2.equals("scissors")) {
-            return "Player 1 won!";
-        } else if (player2.equals("rock") && player1.equals("scissors")) {
-            return "Player 2 won!";
+        Set<String> set = new HashSet<>(Arrays.asList(player1, player2));
+
+        if (set.containsAll(new HashSet<>(Arrays.asList("rock", "scissors")))) {
+            if (player1.equals("rock")) {
+                return "Player 1 won!";
+            } else {
+                return "Player 2 won!";
+            }
         }
         return "Draw!";
     }
