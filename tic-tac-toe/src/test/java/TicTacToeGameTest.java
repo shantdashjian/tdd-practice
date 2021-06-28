c9ead5706534c10241f9a037e84c97103825f323
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 11:14:35 -0700

    ttt: refactor

--- tic-tac-toe/src/test/java/TicTacToeGameTest.java
+++ tic-tac-toe/src/test/java/TicTacToeGameTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -6,15 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeGameTest {
 
+    private static TicTacToeGame game;
+
+    @BeforeAll
+    static void setup() {
+        game = new TicTacToeGame();
+    }
+
     @Test
     void return_0_when_sending_an_empty_board() {
-        TicTacToeGame game = new TicTacToeGame();
         assertEquals(0, game.solveTTT(Arrays.asList("", "", "", "", "", "", "", "", "")));
     }
 
     @Test
     void return_1_when_sending_a_board_with_spot_0_occupied() {
-        TicTacToeGame game = new TicTacToeGame();
         assertEquals(1, game.solveTTT(Arrays.asList("O", "", "", "", "", "", "", "", "")));
     }
 }
