37aca98b6993e523d0d371084736a635f177b2db
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 14:01:01 -0700

    bp: refactor

--- bowling-pins/src/test/java/BowlingTest.java
+++ bowling-pins/src/test/java/BowlingTest.java
@@ -7,8 +7,8 @@ class BowlingTest {
     @Test
     void return_field_with_no_pins_removed() {
         Bowling bowling = new Bowling();
-        int[] testArray = {};
-        assertEquals("I I I I\n I I I \n  I I  \n   I   ", bowling.bowlingPins(testArray));
+        int[] pinsToRemove = {};
+        assertEquals("I I I I\n I I I \n  I I  \n   I   ", bowling.bowlingPins(pinsToRemove));
     }
 
 }
