4447950339ca03343d0dd4ab1089ee53aaa50328
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 11:41:51 -0700

    ttt: get the test to pass

--- tic-tac-toe/src/main/java/TicTacToeGame.java
+++ tic-tac-toe/src/main/java/TicTacToeGame.java
@@ -3,13 +3,10 @@ import java.util.List;
 public class TicTacToeGame {
 
     public int solveTTT(List<String> board) {
-        if (!board.get(0).equals("") && board.get(1).equals("")) {
-            return 1;
+        int index = 0;
+        while (!board.get(index).equals("")) {
+            index++;
         }
-
-        if (!board.get(1).equals("")) {
-            return 2;
-        }
-        return 0;
+        return index;
     }
 }
