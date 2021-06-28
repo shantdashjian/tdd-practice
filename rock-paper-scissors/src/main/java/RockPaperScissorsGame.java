2f2b0a68db03d1c2a2d45fab1e1ef1cade69e115
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 09:24:20 -0700

    rps: get the test to pass

--- rock-paper-scissors/src/main/java/RockPaperScissorsGame.java
+++ rock-paper-scissors/src/main/java/RockPaperScissorsGame.java
@@ -2,6 +2,8 @@ public class RockPaperScissorsGame {
     public String play(String player1, String player2) {
         if (player1.equals("rock") && player2.equals("scissors")) {
             return "Player 1 won!";
+        } else if (player2.equals("rock") && player1.equals("scissors")) {
+            return "Player 2 won!";
         }
         return "Draw!";
     }
