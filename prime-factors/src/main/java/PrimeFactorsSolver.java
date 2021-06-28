b028ee2ac9033421f28b76ef77f781af201763ca
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 16:35:36 -0700

    pf: get the test to pass

--- prime-factors/src/main/java/PrimeFactorsSolver.java
+++ prime-factors/src/main/java/PrimeFactorsSolver.java
@@ -5,12 +5,13 @@ public class PrimeFactorsSolver {
     public List<Integer> primeFactors(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
 
-        while (n % 2 == 0) {
-            n = n / 2;
-            factors.add(2);
-        }
-        if (n > 1) {
-            factors.add(n);
+        int factor = 2;
+        while (n > 1) {
+            while (n % factor == 0) {
+                n = n / factor;
+                factors.add(factor);
+            }
+            factor++;
         }
         return factors;
     }
