9de9e5ee9e4671a282b1e6857468b47fbbaf9506
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 12:40:40 -0700

    ttt: refactor

--- tic-tac-toe/src/test/java/TicTacToeGameTest.java
+++ tic-tac-toe/src/test/java/TicTacToeGameTest.java
@@ -40,38 +40,32 @@ class TicTacToeGameTest {
 
     @Test
     void return_a_winning_index_at_2_when_sending_a_board_with_spots_0_and_1_with_X_and_2_is_empty() {
-        int result = game.solveTTT(Arrays.asList("X", "X", "", "O", "O", "X", "O", "X", "O"));
-        assertEquals(2, result);
+        assertEquals(2, game.solveTTT(Arrays.asList("X", "X", "", "O", "O", "X", "O", "X", "O")));
     }
 
     @Test
     void return_a_winning_index_at_5_when_sending_a_board_with_spots_0_and_1_with_O_and_3_and_4_with_X_and_2_and_5_are_empty() {
-        int result = game.solveTTT(Arrays.asList("O", "O", "", "X", "X", "", "O", "X", "O"));
-        assertEquals(5, result);
+        assertEquals(5, game.solveTTT(Arrays.asList("O", "O", "", "X", "X", "", "O", "X", "O")));
     }
 
     @Test
     void return_a_winning_index_at_4_when_sending_a_board_with_spots_0_and_2_with_O_and_3_and_5_with_X_and_1_and_4_are_empty() {
-        int result = game.solveTTT(Arrays.asList("O", "", "O", "X", "", "X", "O", "X", "O"));
-        assertEquals(4, result);
+        assertEquals(4, game.solveTTT(Arrays.asList("O", "", "O", "X", "", "X", "O", "X", "O")));
     }
 
     @Test
     void return_a_winning_index_at_6_when_sending_a_board_with_spots_6_empty_and_spots_7_and_8_with_X() {
-        int result = game.solveTTT(Arrays.asList("O", "X", "O", "O", "O", "", "", "X", "X"));
-        assertEquals(6, result);
+        assertEquals(6, game.solveTTT(Arrays.asList("O", "X", "O", "O", "O", "", "", "X", "X")));
     }
 
     @Test
     void return_a_winning_index_at_3_when_sending_a_board_with_spot_3_empty_and_0_and_6_X() {
-        int result = game.solveTTT(Arrays.asList("X", "O", "", "", "O", "", "X", "O", "X"));
-        assertEquals(3, result);
+        assertEquals(3, game.solveTTT(Arrays.asList("X", "O", "", "", "O", "", "X", "O", "X")));
     }
 
     @Test
     void return_a_winning_index_at_7_when_sending_a_board_with_spot_7_empty_and_1_and_4_X() {
-        int result = game.solveTTT(Arrays.asList("", "X", "", "", "X", "", "O", "", "O"));
-        assertEquals(7, result);
+        assertEquals(7, game.solveTTT(Arrays.asList("", "X", "", "", "X", "", "O", "", "O")));
     }
 
     @Test
