36d032bd8677c092532e28c4369fa082fcba8933
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 15:25:01 -0700

    bp: refactor

--- bowling-pins/src/main/java/Bowling.java
+++ bowling-pins/src/main/java/Bowling.java
@@ -2,71 +2,36 @@ import java.util.*;
 
 public class Bowling {
     public String bowlingPins(int[] pinsToRemove) {
+        List<Integer> list = getIntegers(pinsToRemove);
+
+        String field = "";
+
+        field += list.contains(7) ? "  " : "I ";
+        field += list.contains(8) ? "  " : "I ";
+        field += list.contains(9) ? "  " : "I ";
+        field += list.contains(10) ? " " : "I";
+        field += "\n ";
+
+        field += list.contains(4) ? "  " : "I ";
+        field += list.contains(5) ? "  " : "I ";
+        field += list.contains(6) ? " " : "I";
+        field += " \n  ";
+
+        field += list.contains(2) ? "  " : "I ";
+        field += list.contains(3) ? " " : "I";
+        field += "  \n   ";
+
+        field += list.contains(1) ? " " : "I";
+        field += "   ";
+
+        return field;
+    }
+
+    private List<Integer> getIntegers(int[] pinsToRemove) {
         List<Integer> list = new ArrayList<>();
         for (int item : pinsToRemove) {
             list.add(item);
         }
-
-        String field = "";
-
-        if (list.contains(7)) {
-            field += "  ";
-        } else {
-            field += "I ";
-        }
-
-        if (list.contains(8)) {
-            field += "  ";
-        } else {
-            field += "I ";
-        }
-
-        if (list.contains(9)) {
-            field += "  ";
-        } else {
-            field += "I ";
-        }
-
-        if (list.contains(10)) {
-            field += " \n ";
-        } else {
-            field += "I\n ";
-        }
-
-        if (list.contains(4)) {
-            field += "  ";
-        } else {
-            field += "I ";
-        }
-
-        if (list.contains(5)) {
-            field += "  ";
-        } else {
-            field += "I ";
-        }
-
-        if (list.contains(6)) {
-            field += "  \n  ";
-        } else {
-            field += "I \n  ";
-        }
-
-        if (list.contains(2)) {
-            field += "  ";
-        } else {
-            field += "I ";
-        }
-        if (list.contains(3)) {
-            field += "   \n   ";
-        } else {
-            field += "I  \n   ";
-        }
-        if (list.contains(1)) {
-            field += "    ";
-        } else {
-            field += "I   ";
-        }
-
-        return field;
+        return list;
     }
 }
