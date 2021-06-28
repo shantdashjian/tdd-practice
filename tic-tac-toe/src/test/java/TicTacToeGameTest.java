195e72fe69f4e888778a7ce5c39ed1780e6862c6
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 11:50:57 -0700

    ttt: add another test, passes

--- tic-tac-toe/src/test/java/TicTacToeGameTest.java
+++ tic-tac-toe/src/test/java/TicTacToeGameTest.java
@@ -37,4 +37,10 @@ class TicTacToeGameTest {
         int result = game.solveTTT(Arrays.asList("O", "X", "O", "", "", "", "", "", ""));
         assertTrue(Arrays.asList(3, 4, 5, 6, 7, 8).contains(result));
     }
+
+    @Test
+    void return_a_winning_index_at_2_when_sending_a_board_with_spots_0_and_1_with_X_and_2_is_empty() {
+        int result = game.solveTTT(Arrays.asList("X", "X", "", "O", "O", "X", "O", "X", "O"));
+        assertEquals(2, result);
+    }
 }
