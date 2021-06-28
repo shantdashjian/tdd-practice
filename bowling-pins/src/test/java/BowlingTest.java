c7a80c305a9f52a4bd396932119b71435039635c
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 14:03:11 -0700

    bp: refactor and add test, fails

--- bowling-pins/src/test/java/BowlingTest.java
+++ bowling-pins/src/test/java/BowlingTest.java
@@ -5,10 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class BowlingTest {
 
     @Test
-    void return_field_with_no_pins_removed() {
+    void return_field_with_no_pins_removed_when_sending_an_empty_array() {
         Bowling bowling = new Bowling();
         int[] pinsToRemove = {};
         assertEquals("I I I I\n I I I \n  I I  \n   I   ", bowling.bowlingPins(pinsToRemove));
+    }
+
+    @Test
+    void return_field_with_pin_1_removed_when_sending_an_array_of_1() {
+        Bowling bowling = new Bowling();
+        int[] pinsToRemove = {1};
+        assertEquals("I I I I\n I I I \n  I I  \n       ", bowling.bowlingPins(pinsToRemove));
     }
 
 }
