2da9740295969c8be96b523296a402285c90d8ce
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 12:48:08 -0700

    ttt: refactor

--- tic-tac-toe/src/test/java/TicTacToeGameTest.java
+++ tic-tac-toe/src/test/java/TicTacToeGameTest.java
@@ -21,26 +21,13 @@ class TicTacToeGameTest {
     }
 
     @Test
-    void return_index_between_1_and_8_when_sending_a_board_with_spot_0_occupied() {
-        int result = game.solveTTT(Arrays.asList("O", "", "", "", "", "", "", "", ""));
-        assertTrue(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8).contains(result));
-    }
-
-    @Test
-    void return_index_between_2_and_8_when_sending_a_board_with_spot_0_and_1_occupied() {
-        int result = game.solveTTT(Arrays.asList("O", "X", "", "", "", "", "", "", ""));
-        assertTrue(Arrays.asList(2, 3, 4, 5, 6, 7, 8).contains(result));
-    }
-
-    @Test
-    void return_index_between_3_and_8_when_sending_a_board_with_spot_0_1_and_2_occupied() {
-        int result = game.solveTTT(Arrays.asList("O", "X", "O", "", "", "", "", "", ""));
-        assertTrue(Arrays.asList(3, 4, 5, 6, 7, 8).contains(result));
-    }
-
-    @Test
-    void return_a_winning_index_at_2_when_sending_a_board_with_spots_0_and_1_with_X_and_2_is_empty() {
-        assertEquals(2, game.solveTTT(Arrays.asList("X", "X", "", "O", "O", "X", "O", "X", "O")));
+    void return_first_non_winning_move() {
+        assertTrue(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+                .contains(game.solveTTT(Arrays.asList("O", "", "", "", "", "", "", "", ""))));
+        assertTrue(Arrays.asList(2, 3, 4, 5, 6, 7, 8)
+                .contains(game.solveTTT(Arrays.asList("O", "X", "", "", "", "", "", "", ""))));
+        assertTrue(Arrays.asList(3, 4, 5, 6, 7, 8)
+                .contains(game.solveTTT(Arrays.asList("O", "X", "O", "", "", "", "", "", ""))));
     }
 
     @Test
