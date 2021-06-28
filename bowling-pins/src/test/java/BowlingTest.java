8086ee5c5b2c5d357b62fd63db7a2ff0bd77019d
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 14:10:44 -0700

    bp: add another test, fails

--- bowling-pins/src/test/java/BowlingTest.java
+++ bowling-pins/src/test/java/BowlingTest.java
@@ -24,4 +24,10 @@ class BowlingTest {
         assertEquals("I I I I\n I I I \n  I I  \n       ", bowling.bowlingPins(pinsToRemove));
     }
 
+    @Test
+    void return_field_with_pin_2_removed_when_sending_an_array_of_2() {
+        int[] pinsToRemove = {2};
+        assertEquals("I I I I\n I I I \n    I  \n   I   ", bowling.bowlingPins(pinsToRemove));
+    }
+
 }
