b5bd85baa661d223f1f8aafd46863fa60c4c7649
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-28 09:18:42 -0700

    rps: refactor

--- rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
+++ rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
@@ -1,12 +1,19 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RockPaperScissorsGameTest {
 
+    private static RockPaperScissorsGame game;
+
+    @BeforeAll
+    static void setup() {
+        game = new RockPaperScissorsGame();
+    }
+
     @Test
     void return_draw_when_player1_is_the_same_as_player2() {
-        RockPaperScissorsGame game = new RockPaperScissorsGame();
         assertEquals("Draw!", game.play("rock", "rock"));
         assertEquals("Draw!", game.play("paper", "paper"));
     }
@@ -14,7 +21,6 @@ class RockPaperScissorsGameTest {
 
     @Test
     void return_player1_when_player1_is_rock_and_player2_is_scissors() {
-        RockPaperScissorsGame game = new RockPaperScissorsGame();
         assertEquals("Player 1 won!", game.play("rock", "scissors"));
     }
 }
