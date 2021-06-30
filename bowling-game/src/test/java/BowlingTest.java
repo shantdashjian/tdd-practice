2ca1b6a33e735e6de8acf9ca50933af3971c301d
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-30 09:30:11 -0700

    BG: Refactor tests

--- bowling-game/src/test/java/BowlingTest.java
+++ bowling-game/src/test/java/BowlingTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingTest {
 
+    public static final int HIT_ALL_PINS = 10;
+    public static final int MISS_ALL_PINS = 0;
     private Game game;
     private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
@@ -49,13 +51,13 @@ public class BowlingTest {
         rollStrike();
         game.roll(3);
         game.roll(4);
-        rollMany(16, 0);
+        rollMany(16, MISS_ALL_PINS);
         assertEquals(10 + 3 + 4 + 3 + 4, game.score());
     }
 
     @Test
     void roll_perfect_game() {
-        rollMany(12, 10);
+        rollMany(12, HIT_ALL_PINS);
         assertEquals(300, game.score());
     }
 }
