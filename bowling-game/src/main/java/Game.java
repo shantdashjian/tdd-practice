83c0a402222dde8e78b989df6199a90c9f8ff469
Shaun Dashjian <shaundashjian@yahoo.com>
2021-06-30 15:59:02 -0700

    BG: Refactor further to keep functions small

--- bowling-game/src/main/java/Game.java
+++ bowling-game/src/main/java/Game.java
@@ -1,73 +1,62 @@
-import lombok.Data;
-
 public class Game {
     public static final int NUMBER_OF_FRAMES = 10;
     public static final int MAX_NUMBER_OF_ROLLS = 21;
     private int[] rolls = new int[MAX_NUMBER_OF_ROLLS];
     private int currentRoll = 0;
+    private int score;
+    private int firstInFrame;
 
     public void roll(int pins) {
         rolls[currentRoll++] = pins;
     }
 
     public int score() {
-        ScoreAndFirstInFrame scoreAndFirstInFrame = new ScoreAndFirstInFrame(0, 0);
+        score = 0;
+        firstInFrame = 0;
         for (int frame = 0; frame < NUMBER_OF_FRAMES; frame++) {
-            if (isStrike(scoreAndFirstInFrame)) {
-                scoreAndFirstInFrame = getScoreAndFirstInFrameForStrike(scoreAndFirstInFrame);
-            } else if (isSpare(scoreAndFirstInFrame)) {
-                scoreAndFirstInFrame = getScoreAndFirstInFrameForSpare(scoreAndFirstInFrame);
+            if (isStrike()) {
+                updateScoreAndFirstInFrameForStrike();
+            } else if (isSpare()) {
+                updateScoreAndFirstInFrameForSpare();
             } else {
-                scoreAndFirstInFrame = getScoreAndFirstInFrameForIncompleteFrame(scoreAndFirstInFrame);
+                updateScoreAndFirstInFrameForIncompleteFrame();
             }
         }
-        return scoreAndFirstInFrame.getScore();
+        return score;
     }
 
-    private ScoreAndFirstInFrame getScoreAndFirstInFrameForIncompleteFrame(ScoreAndFirstInFrame scoreAndFirstInFrame) {
-        return new ScoreAndFirstInFrame(
-                scoreAndFirstInFrame.getScore() + twoRollsInFrame(scoreAndFirstInFrame.getFirstInFrame())
-                , scoreAndFirstInFrame.getFirstInFrame() + 2);
+    private void updateScoreAndFirstInFrameForIncompleteFrame() {
+        score += twoRollsInFrame();
+        firstInFrame += 2;
     }
 
-    private ScoreAndFirstInFrame getScoreAndFirstInFrameForSpare(ScoreAndFirstInFrame scoreAndFirstInFrame) {
-        return new ScoreAndFirstInFrame(
-                scoreAndFirstInFrame.getScore() + Roll.SPARE.getValue() + nextRoll(scoreAndFirstInFrame.getFirstInFrame())
-                , scoreAndFirstInFrame.getFirstInFrame() + 2);
+    private void updateScoreAndFirstInFrameForSpare() {
+        score += Roll.SPARE.getValue() + nextRoll();
+        firstInFrame += 2;
     }
 
-    private ScoreAndFirstInFrame getScoreAndFirstInFrameForStrike(ScoreAndFirstInFrame scoreAndFirstInFrame) {
-        return new ScoreAndFirstInFrame(
-                scoreAndFirstInFrame.getScore() + Roll.STRIKE.getValue() + nextTwoRolls(scoreAndFirstInFrame.getFirstInFrame())
-                , scoreAndFirstInFrame.getFirstInFrame() + 1);
+    private void updateScoreAndFirstInFrameForStrike() {
+        score += + Roll.STRIKE.getValue() + nextTwoRolls();
+        firstInFrame += 1;
     }
 
-
-    private int twoRollsInFrame(int firstInFrame) {
+    private int twoRollsInFrame() {
         return rolls[firstInFrame] + rolls[firstInFrame + 1];
     }
 
-    private int nextRoll(int firstInFrame) {
+    private int nextRoll() {
         return rolls[firstInFrame + 2];
     }
 
-    private int nextTwoRolls(int firstInFrame) {
-        return rolls[firstInFrame + 1] + nextRoll(firstInFrame);
+    private int nextTwoRolls() {
+        return rolls[firstInFrame + 1] + nextRoll();
     }
 
-    private boolean isStrike(ScoreAndFirstInFrame scoreAndFirstInFrame) {
-        return rolls[scoreAndFirstInFrame.getFirstInFrame()]
-                == Roll.STRIKE.getValue();
+    private boolean isStrike() {
+        return rolls[firstInFrame] == Roll.STRIKE.getValue();
     }
 
-    private boolean isSpare(ScoreAndFirstInFrame scoreAndFirstInFrame) {
-        return twoRollsInFrame(scoreAndFirstInFrame.getFirstInFrame())
-                == Roll.SPARE.getValue();
+    private boolean isSpare() {
+        return twoRollsInFrame() == Roll.SPARE.getValue();
     }
-}
-
-@Data
-class ScoreAndFirstInFrame {
-    private final int score;
-    private final int firstInFrame;
 }
