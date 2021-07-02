22ead50dd774b35da7f7fffcece041bbd9c81317
Shaun Dashjian <shaundashjian@yahoo.com>
2021-07-02 10:43:19 -0700

    String Set: Improve test for add, refactor

--- string-set/src/test/java/StringSetTest.java
+++ string-set/src/test/java/StringSetTest.java
@@ -32,12 +32,13 @@ public class StringSetTest {
     }
 
     @Test
-    void return_count_1_when_adding_string_to_set() {
+    void test_adding_string_to_set() {
         String stringAdded = "Tim";
 
         set.add(stringAdded);
 
         assertThat(set.getCount()).isEqualTo(1);
+        assertThat(set.contains(stringAdded)).isTrue();
     }
 
     @Test
