eb34229a21d82756573278909474fb2c8511d5b1
Shaun Dashjian <shaundashjian@yahoo.com>
2021-07-02 08:38:30 -0700

    String Set: Add test to create set, get it to pass

---
+++ string-set/src/test/java/StringSetTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class StringSetTest {
+
+    /* String set
+        Develop a class that represents a string set, supporting set operations:
+            Add string
+            Checking if the set contains a specific string
+            Remove string
+            Number of strings in the set, Count
+            Union of two sets
+            Intersection of two sets.
+            Clearing the set.
+            Enumerating the set.
+
+     */
+
+    @Test
+    void can_create_set() {
+        StringSet set = new StringSet();
+    }
+}
