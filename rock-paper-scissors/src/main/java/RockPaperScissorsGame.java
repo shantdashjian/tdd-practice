ea84f688eaac169ddf26635ae626f2b0b76eb4ab
shantdashjian <shantgdashjian@gmail.com>
2021-07-13 09:40:53 -0700

    RPS: Add another test, get it to pass, refactor

--- rock-paper-scissors/src/main/java/RockPaperScissorsGame.java
+++ rock-paper-scissors/src/main/java/RockPaperScissorsGame.java
@@ -1,5 +1,8 @@
 public class RockPaperScissorsGame {
     public String play(String player1, String player2) {
+        if (player1.equals("paper") && player2.equals("scissors")) {
+            return "Player Two Wins!";
+        }
         return "Player One Wins!";
     }
 }
--- rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
+++ rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -5,12 +7,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RockPaperScissorsGameTest {
 
+    private RockPaperScissorsGame game;
+
+    @BeforeEach
+    void setup() {
+        game = new RockPaperScissorsGame();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"paper", "Paper", "PAPER"})
     void return_player_one_wins_when_p1_plays_paper_with_different_char_cases_and_p2_plays_rock(String player1) {
-        RockPaperScissorsGame game = new RockPaperScissorsGame();
         String player2 = "rock";
         String expectedResult = "Player One Wins!";
+
+        String actualResult = game.play(player1, player2);
+
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void return_player_two_wins_when_p1_plays_paper_and_p2_plays_scissors() {
+        String player1 = "paper";
+        String player2 = "scissors";
+        String expectedResult = "Player Two Wins!";
 
         String actualResult = game.play(player1, player2);
 
