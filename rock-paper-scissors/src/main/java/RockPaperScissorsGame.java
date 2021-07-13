488486dbd214a746c98f0ff247f74cc5479d2204
shantdashjian <shantgdashjian@gmail.com>
2021-07-13 09:27:27 -0700

    RPC: Add first test, get it to pass

---
+++ rock-paper-scissors/src/main/java/RockPaperScissorsGame.java
@@ -0,0 +1,5 @@
+public class RockPaperScissorsGame {
+    public String play(String player1, String player2) {
+        return "Player One Wins!";
+    }
+}
--- rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
+++ rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
@@ -1,3 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RockPaperScissorsGameTest {
 
+    @Test
+    void return_player_one_wins_when_p1_plays_paper_and_p2_plays_rock() {
+        RockPaperScissorsGame game = new RockPaperScissorsGame();
+        String player1 = "paper";
+        String player2 = "rock";
+        String expectedResult = "Player One Wins!";
+
+        String actualResult = game.play(player1, player2);
+
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
 }
