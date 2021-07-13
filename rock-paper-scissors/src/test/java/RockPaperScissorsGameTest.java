aaafe3d8d88d0f8fab26f19a8f859df36e494193
shantdashjian <shantgdashjian@gmail.com>
2021-07-13 09:32:20 -0700

    RPC: Make test parameterized to handle various char cases

--- rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
+++ rock-paper-scissors/src/test/java/RockPaperScissorsGameTest.java
@@ -1,13 +1,14 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RockPaperScissorsGameTest {
 
-    @Test
-    void return_player_one_wins_when_p1_plays_paper_and_p2_plays_rock() {
+    @ParameterizedTest
+    @ValueSource(strings = {"paper", "Paper", "PAPER"})
+    void return_player_one_wins_when_p1_plays_paper_with_different_char_cases_and_p2_plays_rock(String player1) {
         RockPaperScissorsGame game = new RockPaperScissorsGame();
-        String player1 = "paper";
         String player2 = "rock";
         String expectedResult = "Player One Wins!";
 
